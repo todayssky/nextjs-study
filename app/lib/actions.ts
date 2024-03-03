0c09ec7cbb6460b5f99cb1bf934cbbb4706946b2
todayssky <rudwns1113@gmail.com>
2024-03-03 15:22:27 +0900

    인증 추가

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
+import { signIn } from '@/auth';
 import { sql } from '@vercel/postgres';
+import { AuthError } from 'next-auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
@@ -107,15 +109,33 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Database Error!!');
-  // try {
-  //   await sql`
-  //   DELETE FROM invoices WHERE id = ${id}
-  // `;
-  // } catch (error) {
-  //   return {
-  //     message: 'Database Error: Failed to Delete Invoice.',
-  //   };
-  // }
-  // revalidatePath('/dashboard/invoices');
+  try {
+    await sql`
+    DELETE FROM invoices WHERE id = ${id}
+  `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Delete Invoice.',
+    };
+  }
+  revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
